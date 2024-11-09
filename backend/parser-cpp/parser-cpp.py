cea1f97275ab8f3276c8aa11abec4bc05441850a
leo-cabezas-amigo <l859c248@ku.edu>
2024-11-09 10:59:24 -0600

    Basic parsing

--- backend/parser-cpp/parser-cpp.py
+++ backend/parser-cpp/parser-cpp.py
@@ -53,12 +53,65 @@ class Parser_CPP:
         self.json_dict = {"code": self.code, "executionSteps": []}
         self.jsonResponse = ""
 
+        """
+        self.cpp_keywords_dict = {
+            "if"        : self._parse_if,
+            "else"      : self._parse_else,
+            "switch"    : self._parse_switch,
+            "case"      : self._parse_case,
+            "default"   : self._parse_default,
+            "for"       : self._parse_for,
+            "while"     : self._parse_while,
+            "do"        : self._parse_do,
+            "break"     : self._parse_break,
+            "continue"  : self._parse_continue,
+            "return"    : self._parse_return,
+            "goto"      : self._parse_goto,
+            "throw"     : self._parse_throw,
+            "try"       : self._parse_try,
+            "catch"     : self._parse_catch
+        }
+        """
+
+        self.scope_stack = []
+        self.scope_stack.append(["__SCOPE__GLOBAL__", len(self.code_lines), {}])
+        self.scope_registry = [] 
+    
     def _read_src(self, file, byLine = False):  # DONE!
         try:
             file.seek(0)
             return file.read() if not byLine else file.readlines()
         except:
             raise Exception("Error. Could not open src_code file.")
+        
+    def _parse_dtype(self, dtype, i, line, step):   # DONE!
+        LOOP_COND = lambda i: i < len(self.code)
+        
+        i += len(dtype) + 1
+        highlight = line
+        
+        while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+            i += 1
+        name_start = i
+        while self.code[i] not in next_chars["end_dtype"] and LOOP_COND(i):
+            i += 1
+        name_end = i
+        name = self.code[name_start:name_end]
+
+        while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+            i += 1
+
+        argv = [i, line, step, highlight, name, dtype]
+        if self.code[i] in ["=", ";"]: # Means it's VAR_DECLARE
+            i, line = self._parse_VAR_DECLARE(argv)
+        elif self.code[i] == "(":   # Means it's FUNCT_DEFINE
+            i, line = self._parse_FUNCT_DEFINE(argv)
+        else:
+            raise Exception("Error! Unknown dtype-init statement.")
+        
+        step += 1
+
+        return i, line, step
 
 if __name__ == "__main__":
     code_file = open("ideal_parser_test.cpp", 'r')
