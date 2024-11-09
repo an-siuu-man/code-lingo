4186fd7921d58b5ed1a750ef6c7979de15f9bd6b
leo-cabezas-amigo <l859c248@ku.edu>
2024-11-09 15:36:03 -0600

    Push rest of untested backlogged code

--- backend/parser-cpp/parser-cpp.py
+++ backend/parser-cpp/parser-cpp.py
@@ -428,7 +428,587 @@ class Parser_CPP:
             
             return i, line
 
+    def _parse_VAR_DECLARE(self, argv: list):   # DONE!
+        LOOP_COND, i, line, step, highlight, name, data_type = argv
+        
+        if self.code[i] == "=":
+            i += len("=")
+            while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+                i += 1
+            value_start = i
+            while self.code[i] != ";" and LOOP_COND(i):
+                i += 1
+            value_end = i
+            value = self.code[value_start:value_end]
+        else:   # self.code[i] == ";"
+            value = "__UNINITIALIZED__"   # Uninitialized variable case
+        
+        reference = "__DECLAREDAT__" + str(highlight) + "__NAME__" + name
+
+        self.scope_stack[-1][2][reference] =  [data_type, name, value]
+        if value != "__UNINITIALIZED__":
+            # Temporary fix, implement scalable solution
+            if value == "true":
+                value = "True"
+            elif value == "false":
+                value = "False"
+            
+            exec(f"{reference} = {value}", globals())
+            # print(eval(reference))
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "VAR_DECLARE",
+                "reference" : reference,
+                "type"      : data_type,
+                "name"      : name,
+                "value"     : value
+            }
+        )
+        self.json_dict["executionSteps"] = executionSteps
+        
+        step += 1
+
+        return i, line, step
+
+    def _parse_VAR_UPDATE(self, argv: list):  # DONE!
+        LOOP_COND, i, line, step, highlight, reference, stmt_start = argv
+
+        while self.code[i] != ";" and LOOP_COND(i):
+            i += 1
+        stmt_end = i
+        
+        update_stmt = self.code[stmt_start:stmt_end]
+        #update_stmt = update_stmt.replace("=", " = ")
+        update_stmt_list = self.code[stmt_start:stmt_end].split()
+        
+        # print(f"update_stmt_list = {update_stmt_list}")
+        for scope in self.scope_stack[::-1]:
+            for ref in scope[2].keys():
+                update_stmt_list = [ref if scope[2][ref][1] == x else x for x in update_stmt_list]
+
+                # Quick fix, find scalable solution
+                update_stmt_list = ["False" if x == "false" else x for x in update_stmt_list]
+                update_stmt_list = ["True" if x == "true" else x for x in update_stmt_list]
+            try:
+                data_type = scope[2][reference][0]
+                name = scope[2][reference][1]
+                old_value = scope[2][reference][2]
+
+                target_scope = scope
+            except:
+                pass
+        # print(f"update_stmt_list = {update_stmt_list}")
+        # print()
+        ref_update_stmt = " ".join(update_stmt_list)
+
+        exec(ref_update_stmt, globals())
+        new_value = str(eval(reference))
+        target_scope[2][reference][2] = new_value
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "VAR_UPDATE",
+                "reference" : reference,
+                "type"      : data_type,  
+                "name"      : name,
+                "statement" : update_stmt,
+                "old_value" : old_value,
+                "new_value" : new_value
+            },
+        )
+        self.json_dict["executionSteps"] = executionSteps
+        
+        return i, line
+    
+    def _parse_if(self, argv: list):    # DONE!
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+            i += 1
+        if self.code[i] == "(":
+            i += len("(")
+        cond_start = i
+        while self.code[i] != ")" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        cond_end = i
+        
+        condition = self.code[cond_start:cond_end]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        eval_stmt_list = condition.split()
+
+        for scope in self.scope_stack[::-1]:
+            for ref in scope[2].keys():
+                eval_stmt_list = [ref if scope[2][ref][1] == x else x for x in eval_stmt_list]
+        ref_eval_stmt = " ".join(eval_stmt_list)
+
+        result = str(eval(ref_eval_stmt))
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "IF_BLOCK",
+                "condition" : condition,
+                "result"    : result,
+                "startLine" : startLine,
+                "endLine"   : endLine
+            }
+        )
+        self.json_dict["executionSteps"] = executionSteps
+
+        scope_reference = f"__DECLAREDAT__{highlight}__IF_BLOCK__"
+        self.scope_stack.append([scope_reference, endLine, {}])
+
+        step += 1
+
+        return backtrack_i, startLine, step
+
+    def _parse_else(self, argv: list):  # DONE!
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "ELSE_BLOCK",
+                "startLine" : startLine,
+                "endLine"   : endLine
+            }
+        )
+        self.json_dict["executionSteps"] = executionSteps
+
+        scope_reference = f"__DECLAREDAT__{highlight}__ELSE_BLOCK__"
+        self.scope_stack.append([scope_reference, endLine, {}])
+
+        step += 1
+
+        return backtrack_i, startLine, step
+
+    def _parse_switch(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == ";": # Function declaration, not definition!
+                return i, line, step
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        return i, endLine, step
+
+    def _parse_case(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_default(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_for(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == ";": # Function declaration, not definition!
+                return i, line, step
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        return i, endLine, step
+
+    def _temp_parse_for(self, argv: list):  # IN PROGRESS!
+        LOOP_COND, i, line, step, highlight, start_for_i, start_for_line = argv[:7]
+        
+        scope_reference = f"__DECLAREDAT__{highlight}__FOR_LOOP__"
+        if self.scope_stack[-1][0] == scope_reference:
+            loop_params = self.scope_stack[-1][3]
+            ref_eval_stmt, in_for_i, in_for_line, end_for_i, end_for_line = loop_params
+            raw_result = eval(ref_eval_stmt)
+
+            if raw_result:
+                i, line, step = self._parse_section(in_for_i, in_for_line, step, end_for_i, end_for_line)
+                return start_for_i - 1, start_for_line, step
+            else:
+                return end_for_i, end_for_line, step
+
+        while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+            i += 1
+        if self.code[i] == "(":
+            i += len("(")
+        stmts_start = i
+        while self.code[i] != ")" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        stmts_end = i
+        
+        statements = self.code[stmts_start:stmts_end]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        eval_stmt_list = statements.split()
+
+        for scope in self.scope_stack[::-1]:
+            for ref in scope[2].keys():
+                eval_stmt_list = [ref if scope[2][ref][1] == x else x for x in eval_stmt_list]
+                
+                # Quick fix, find scalable solution
+                eval_stmt_list = ["False" if x == "false" else x for x in eval_stmt_list]
+                eval_stmt_list = ["True" if x == "true" else x for x in eval_stmt_list]
+        ref_eval_stmt = " ".join(eval_stmt_list)
+
+        raw_result = eval(ref_eval_stmt)
+        result = str(raw_result)
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "FOR_LOOP",
+                "statements": statements,
+                "condition" : condition,
+                "result"    : result,
+                "startLine" : startLine,
+                "endLine"   : endLine
+            }
+        )
+        self.json_dict["executionSteps"] = executionSteps
+        
+        scope_reference = f"__DECLAREDAT__{highlight}__WHILE_LOOP__"
+
+        in_for_i = backtrack_i
+        in_for_line = startLine
+        end_for_i = i
+        end_for_line = endLine
+        while_loop_params = [ref_eval_stmt, in_for_i, in_for_line, end_for_i, end_for_line]
+        
+        self.scope_stack.append([scope_reference, endLine, {}, while_loop_params])
+
+        step += 1
+
+        if raw_result:
+            return start_while_i - 1, start_while_line, step
+        else:
+            return i, endLine, step
+
+    def _parse_while(self, argv: list): # DONE!
+        LOOP_COND, i, line, step, highlight, start_while_i, start_while_line = argv[:7]
+        
+        scope_reference = f"__DECLAREDAT__{highlight}__WHILE_LOOP__"
+        if self.scope_stack[-1][0] == scope_reference:
+            loop_params = self.scope_stack[-1][3]
+            ref_eval_stmt, in_while_i, in_while_line, end_while_i, end_while_line = loop_params
+            raw_result = eval(ref_eval_stmt)
+
+            if raw_result:
+                i, line, step = self._parse_section(in_while_i, in_while_line, step, end_while_i, end_while_line)
+                return start_while_i - 1, start_while_line, step
+            else:
+                return end_while_i, end_while_line, step
+
+        while self.code[i] in next_chars["line_wspc"] and LOOP_COND(i):
+            i += 1
+        if self.code[i] == "(":
+            i += len("(")
+        cond_start = i
+        while self.code[i] != ")" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        cond_end = i
+        
+        condition = self.code[cond_start:cond_end]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        eval_stmt_list = condition.split()
+
+        for scope in self.scope_stack[::-1]:
+            for ref in scope[2].keys():
+                eval_stmt_list = [ref if scope[2][ref][1] == x else x for x in eval_stmt_list]
+                
+                # Quick fix, find scalable solution
+                eval_stmt_list = ["False" if x == "false" else x for x in eval_stmt_list]
+                eval_stmt_list = ["True" if x == "true" else x for x in eval_stmt_list]
+        ref_eval_stmt = " ".join(eval_stmt_list)
+
+        raw_result = eval(ref_eval_stmt)
+        result = str(raw_result)
+
+        executionSteps = self.json_dict["executionSteps"] 
+        executionSteps.append(
+            {
+                "step"      : step,
+                "highlight" : highlight,
+                "operation" : "WHILE_LOOP",
+                "condition" : condition,
+                "result"    : result,
+                "startLine" : startLine,
+                "endLine"   : endLine
+            }
+        )
+        self.json_dict["executionSteps"] = executionSteps
+        
+        scope_reference = f"__DECLAREDAT__{highlight}__WHILE_LOOP__"
+
+        in_while_i = backtrack_i
+        in_while_line = startLine
+        end_while_i = i
+        end_while_line = endLine
+        while_loop_params = [ref_eval_stmt, in_while_i, in_while_line, end_while_i, end_while_line]
+        
+        self.scope_stack.append([scope_reference, endLine, {}, while_loop_params])
+
+        step += 1
+
+        if raw_result:
+            return start_while_i - 1, start_while_line, step
+        else:
+            return i, endLine, step
+
+    def _parse_dowhile(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == ";": # Function declaration, not definition!
+                return i, line, step
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        while self.code[i] != ";":
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+
+        return i, line, step
+
+    def _parse_break(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]        
+        return i, line, step
+
+    def _parse_continue(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_return(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_goto(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_throw(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+        return i, line, step
+
+    def _parse_try(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == ";": # Function declaration, not definition!
+                return i, line, step
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        return i, endLine, step
+
+    def _parse_catch(self, argv: list):
+        LOOP_COND, i, line, step, highlight = argv[:5]
+
+        net_bracket_cnt = 0
+        while self.code[i] != "{" and LOOP_COND(i):
+            if self.code[i] == ";": # Function declaration, not definition!
+                return i, line, step
+            if self.code[i] == "\n":
+                line += 1
+            i += 1
+        backtrack_i = i
+        startLine = line
+        net_bracket_cnt += 1
+
+        while net_bracket_cnt != 0 and LOOP_COND(i):
+            i += 1
+            if self.code[i] == "\n":
+                line += 1
+            
+            if self.code[i] == "{":
+                net_bracket_cnt += 1
+            elif self.code[i] == "}":
+                net_bracket_cnt -= 1
+        endLine = line
+
+        return i, endLine, step
+
+    def generate_json(self):
+        self._pop_COMMENT() # Preprocessing
+        self._pop_INCLUDE() # Preprocessing
+        self._parse_code()  # Actual parsing
+
+        self.jsonResponse = json.dumps(self.json_dict, indent=4)
+        return self.jsonResponse
+
 if __name__ == "__main__":
     code_file = open("ideal_parser_test.cpp", 'r')
 
     parser = Parser_CPP(code_file)
+    parser.generate_json()
+    print()
+    print(f"parser.jsonResponse = \n{parser.jsonResponse}")
+    print()
+    scope_stack = json.dumps(parser.scope_stack, indent=4)
+    print(f"parser.scope_stack = \n{scope_stack}")
+    print()
+    scope_registry = json.dumps(parser.scope_registry, indent=4)
+    print(f"parser.scope_registry = \n{scope_registry}")
+    print()
