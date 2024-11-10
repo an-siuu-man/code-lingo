3f772bb6fad8e6e0501b71ba14bd90925dc4d228
an-siuu-man <ansuman_sharma@protonmail.com>
2024-11-09 19:33:43 -0600

    fixed sidebar on learning page

--- codelingo/src/app/learning/page.js
+++ codelingo/src/app/learning/page.js
@@ -1,43 +1,25 @@
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-
-// export default function Learning() {
-//     return (
-//         <div>
-//             <Navbar />
-//             <h1>Learning Page</h1>
-//         </div>
-//     );
-// }
-
 'use client';
 
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from './Sidebar';
+import EditorMonaco from '../components/EditorMonaco';
 
 export default function Learning() {
     return (
-        <div className='flex'>
+        <div className='relative flex w-full'>
             {/* Sidebar component on the left */}
             <Sidebar />
 
             {/* Main content area for the Learning page */}
-            <div style={{ flexGrow: 1, backgroundColor: '#1e1e1e', color: 'white', minHeight: '100vh' }}>
+            <div className='absolute left-[5vw] w-[95vw] items-right flex-grow' style={{ backgroundColor: '#1e1e1e', color: 'white', minHeight: '100vh' }}>
                 <Navbar />
                 <h1>Learning Page</h1>
                 
                 {/* Placeholder for additional sections or components */}
                 <div>
                     <p>Welcome to the Learning Page. Here, you'll find different coding concepts and interactive modules to learn from.</p>
-
-                    {/* Example content area where new sections/components can be added */}
-                    <section>
-                        <h2>Section Title</h2>
-                        <p>Content for this section goes here. You can add any components or details you need.</p>
-                    </section>
-
-                    {/* Add more sections/components here as needed */}
+                    <EditorMonaco />
                 </div>
             </div>
         </div>
