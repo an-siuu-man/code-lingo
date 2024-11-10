d9a27317fa2d5fa8191189435ff66865a2773502
Jay Patel <jayrpatel2004>
2024-11-10 04:09:22 -0600

    learning ready for visualizer

--- codelingo/src/app/learning/[slug]/page.js
+++ codelingo/src/app/learning/[slug]/page.js
@@ -40,16 +40,13 @@ export default function Learning() {
         <div className='relative flex w-full'>
             {/* Sidebar component on the left */}
             <Sidebar />
+
             {/* Main content area for the Learning page */}
             <div className='absolute left-[5vw] w-[95vw] items-right flex-grow' style={{ backgroundColor: '#1e1e1e', color: 'white', minHeight: '100vh' }}>
-                <Navbar />
-                <h1>Learning Page</h1>
-                <h1>{params.slug}</h1>
-                
-                {/* Placeholder for additional sections or components */}
-                <div>
-                    <p>Welcome to the Learning Page. Here, you'll learn about {incomingJSON.content}</p>
-                    <EditorMonaco readOnly={false} height='500px' incomingCode = {incomingJSON.code ? incomingJSON.code.replace(/\\n/g, '\n') : ''} />
+            <Navbar />
+            <h1>Learning Page</h1>
+                <div className='mr-[2vw] flex-grow p-4 bg-[#2e2e2e] rounded-lg shadow-lg'>
+                    <EditorMonaco  readOnly={false} height='450px' width='50%' incomingCode = {incomingJSON.code ? incomingJSON.code.replace(/\\n/g, '\n') : ''} />
 
                 </div>
             </div>
