564abfcb15f8ae6e91e6e3c6ce1fdf15a6c5183b
an-siuu-man <ansuman_sharma@protonmail.com>
2024-11-09 22:34:17 -0600

    made some changes to incorporate slug routing in sidebar

--- codelingo/src/app/components/Sidebar.js
+++ codelingo/src/app/components/Sidebar.js
@@ -1,15 +1,18 @@
 'use client';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
+
 const Sidebar = (props) => {
   const [topics, setTopics] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  
 
   useEffect(() => {
     const fetchTopics = async () => {
       const res = await axios.get('/api/learningroute/');
       const data = await res.data;
-
+      console.log("DATA: ", res.data);
       const topicsArray = Object.entries(data).map(([key, subtopics]) => ({
         topic: key,
         subheadings: subtopics,
@@ -18,8 +21,9 @@ const Sidebar = (props) => {
       setTopics(topicsArray);
     };
     fetchTopics();
-  }, []);
 
+  }, []);
+  console.log(topics);
   return (
     <nav
       className={`z-[99] learning-sidebar fixed top-0 left-0 h-full overflow-scroll bg-[#333] transition-all duration-300 ${
@@ -27,14 +31,16 @@ const Sidebar = (props) => {
       }`}
     >
       <div
-        className={`text-white cursor-pointer`}
+        className={`flex items-right align-right w-[100%] cursor-pointer`}
         onClick={() => setIsOpen(!isOpen)}
       >
-        <p className='p-2 text-right'>{isOpen ? 'Hide Topics' : 'Show Topics'}</p>
+        {isOpen ? <p className='text-right'>Hide Topics</p> : <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0 0 256 256">
+<g fillOpacity="0" fill="#dddddd" height={'20px'} width={'20px'} fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0"><path d="M0,256v-256h256v256z" id="bgRectangle"></path></g><g fill="#ffffff" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{mixBlendMode: "normal"}}><g transform="scale(3.55556,3.55556)"><path d="M56,48c2.209,0 4,1.791 4,4c0,2.209 -1.791,4 -4,4c-1.202,0 -38.798,0 -40,0c-2.209,0 -4,-1.791 -4,-4c0,-2.209 1.791,-4 4,-4c1.202,0 38.798,0 40,0zM56,32c2.209,0 4,1.791 4,4c0,2.209 -1.791,4 -4,4c-1.202,0 -38.798,0 -40,0c-2.209,0 -4,-1.791 -4,-4c0,-2.209 1.791,-4 4,-4c1.202,0 38.798,0 40,0zM56,16c2.209,0 4,1.791 4,4c0,2.209 -1.791,4 -4,4c-1.202,0 -38.798,0 -40,0c-2.209,0 -4,-1.791 -4,-4c0,-2.209 1.791,-4 4,-4c1.202,0 38.798,0 40,0z"></path></g></g>
+</svg>}
       </div>
       {isOpen && (
         <ul>
-          <h1 className="p-4 text-xl text-center">Learning Topics</h1>
+          <h1 className="py-4 ml-8 text-xl text-left">Learning Topics</h1>
 
           {topics.map((topicObj, index) => (
             <li key={index}>
@@ -50,7 +56,9 @@ const Sidebar = (props) => {
                           </p>
                         </td>
                         <td>
-                          <p className="text-left sub-topic ml-2 text-[15px]">{subheading}</p>
+                          <Link className="text-left sub-topic ml-2 text-[15px]" href={`/learning/${subheading}`}>
+                            {subheading}
+                          </Link>
                         </td>
                       </tr>
                     ))}
